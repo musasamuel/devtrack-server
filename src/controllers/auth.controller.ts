6d6787a9d4a23f3b78789eb5989545432edac6e9
Musa Samuel <samuelgidi29@gmail.com>
2026-02-28 23:20:51 +0100

    feat: add user login endpoint with JWT

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import prisma from "../lib/prisma";
+import jwt from "jsonwebtoken"
 
 export const register = async (req: Request, res: Response) => {
    try { 
@@ -32,4 +33,38 @@ export const register = async (req: Request, res: Response) => {
     console.error(error);
     return res.status(500).json({ message: "Internal server error" });
   }
+};
+export const login = async (req: Request, res: Response) => {
+    try{
+      const { email, password} = req.body
+      if (!email || !password){
+        return res.status(400).json({message: "Email and Password required"})
+      };
+      const existingUser = await prisma.user.findUnique({
+        where: {email}
+    })
+      if(!existingUser){
+        return res.status(400).json({message: "User not found"});
+      }
+    const matchedPassword = await bcrypt.compare(password, existingUser.password);
+      if(!matchedPassword){
+        return res.status(400).json({message: "Invalid password"})
+      }
+      const token = jwt.sign(
+        {userId: existingUser.id},
+        process.env.JWT_SECRET as string,
+        {expiresIn: "1d"}
+      );
+      res.status(200).json({
+        message: "Login Successful",
+        token,
+        user: {
+            id: existingUser.id,
+            email: existingUser.email
+        }
+      })
+    } catch(err){
+        console.log(err);
+        return res.status(500).json({message: "Internal server error"})
+    };
 };
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,8 @@
 import {Router,} from "express";
 import {register} from "../controllers/auth.controller"
+import { login } from "../controllers/auth.controller";
 
 const authRouter = Router();
 authRouter.post("/register", register);
+authRouter.post("/login", login)
 export default authRouter
